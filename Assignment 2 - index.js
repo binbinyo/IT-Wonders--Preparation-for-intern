90434b28f253df3a77ad52ca9449f8240f2c455b
binbinyo <33612780+binbinyo@users.noreply.github.com>
2019-01-29 12:47:15 +0800

    Update Assignment 2 - index.js

--- Assignment 2 - index.js
+++ Assignment 2 - index.js
@@ -58,4 +58,40 @@ new Vue({
     }
 })
 
+Vue.component('messages',{
+  
+  props:['title', 'body'],
+  
+  data(){
+    return{
+      isVisible: true
+    };
+  },
+  
+  template:`
+  <article class="message" v-show="isVisible">
+
+  <div class="message-header">
+
+    {{title}}
+
+<button type="button" @click="hideModal" class="delete" aria-label="delete"></button>
+  </div>
+  <div class="message-body">
+    {{body}}
+  </div>
+</article>`,
+  
+  methods:{
+  hideModal(){
+  this.isVisible = false;
+}
+}
+  
+})
+
+new Vue({
+  el: '#ep9'
+});
+
 
