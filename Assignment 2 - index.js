841fc1897da5d5aff1cd3a7084592d7be10626c1
binbinyo <33612780+binbinyo@users.noreply.github.com>
2019-01-29 15:26:40 +0800

    Update Assignment 2 - index.js

--- Assignment 2 - index.js
+++ Assignment 2 - index.js
@@ -1,97 +1,172 @@
 new Vue({
   el: "#ep1",
   data: () => ({
-    message: 'This is Episode 1'
+    message: "This is Episode 1"
   })
-
 });
 
 new Vue({
-  
   el: "#ep3",
-  
-  data: () => ({ 
-    newName:'',  
-    names: ['Ken', 'Cassandra', 'Steve'],
-    title:'You can only add once',
+
+  data: () => ({
+    newName: "",
+    names: ["Ken", "Cassandra", "Steve"],
+    title: "You can only add once",
     isToggled: false,
     active: false
   }),
-  
-  methods:{
-    addName(){  
+
+  methods: {
+    addName() {
       this.names.push(this.newName);
-      this.newName='';
+      this.newName = "";
       this.isToggled = true;
       this.active = true;
-    },
-    
+    }
   }
 });
 
 new Vue({
-    el: '#ep6',
+  el: "#ep6",
 
-    data: {
-        tasks: [{
-            description: 'Assignment 1',
-            isComplete: false
-        }, {
-            description: 'Assignment 2',
-            isComplete: false
-        }, {
-            description: 'Report on first day',
-            isComplete: false
-        }, {
-            description: 'Do House Chores',
-            isComplete: false
-        }]
+  data: {
+    tasks: [
+      {
+        description: "Assignment 1",
+        isComplete: false
+      },
+      {
+        description: "Assignment 2",
+        isComplete: false
+      },
+      {
+        description: "Report on first day",
+        isComplete: false
+      },
+      {
+        description: "Do House Chores",
+        isComplete: false
+      }
+    ]
+  },
+
+  computed: {
+    incompleteTasks() {
+      return this.tasks.filter(task => !task.isComplete);
     },
-
-    computed: {
-        incompleteTasks() {
-                return this.tasks.filter(task => !task.isComplete)
-        },
-        completeTasks() {
-            return this.tasks.filter(task => task.isComplete)
-        }
+    completeTasks() {
+      return this.tasks.filter(task => task.isComplete);
     }
-})
+  }
+});
 
-Vue.component('messages',{
-  
-  props:['title', 'body'],
-  
-  data(){
-    return{
+Vue.component("messages", {
+  props: ["title", "body"],
+
+  data() {
+    return {
       isVisible: true
     };
   },
-  
-  template:`
-  <article class="message" v-show="isVisible">
 
-  <div class="message-header">
+  template: `
+<article class="message" v-show="isVisible">
 
-    {{title}}
+<div class="message-header">
+
+{{title}}
 
 <button type="button" @click="hideModal" class="delete" aria-label="delete"></button>
-  </div>
-  <div class="message-body">
-    {{body}}
-  </div>
+</div>
+<div class="message-body">
+{{body}}
+</div>
 </article>`,
-  
-  methods:{
-  hideModal(){
-  this.isVisible = false;
-}
-}
-  
-})
 
-new Vue({
-  el: '#ep9'
+  methods: {
+    hideModal() {
+      this.isVisible = false;
+    }
+  }
 });
 
+Vue.component("modal", {
+  template: `
+<div class="modal is-active">
+<div class="modal-background"></div>
+<div class="modal-content">
+<div class="box">
+<p>This is episode 10</p>
+</div>
+</div>
+<button class="modal-close is-large" aria-label="close" @click="$emit('close')"></button>
+</div>`
+});
 
+new Vue({
+  el: "#ep9",
+  data: {
+    showModal: false
+  }
+});
+
+Vue.component("tabs", {
+  template: `
+<div>
+<div class="tabs">
+<ul>
+<li v-for="tab in tabs" :class="{ 'is-active': tab.isActive}">
+<a :href="tab.href" @click="selectTab(tab)">{{ tab.name }}</a>
+</li>
+</ul>
+</div>
+
+<div class="tabs-details">
+<slot></slot>
+</div>
+</div>
+`,
+
+  data() {
+    return { tabs: [] };
+  },
+
+  created() {
+    this.tabs = this.$children;
+  },
+
+  methods: {
+    selectTab(selectedTab) {
+      this.tabs.forEach(tab => {
+        tab.isActive = tab.name == selectedTab.name;
+      });
+    }
+  }
+});
+
+Vue.component("tab", {
+  template: `
+<div v-show="isActive">
+<slot></slot>
+</div>
+`,
+  props: {
+    name: { required: true },
+    selected: { default: false }
+  },
+
+  data() {
+    return {
+      isActive: false
+    };
+  },
+
+  mounted() {
+    this.isActive = this.selected;
+  }
+});
+
+new Vue({
+  el: "#ep11",
+  data: {}
+});
