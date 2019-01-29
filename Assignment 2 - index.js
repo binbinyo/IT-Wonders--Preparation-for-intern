dd68a556856c77333b51987406bc78af1557574d
binbinyo <33612780+binbinyo@users.noreply.github.com>
2019-01-29 15:54:23 +0800

    Update Assignment 2 - index.js

--- Assignment 2 - index.js
+++ Assignment 2 - index.js
@@ -1,65 +1,3 @@
-new Vue({
-  el: "#ep1",
-  data: () => ({
-    message: "This is Episode 1"
-  })
-});
-
-new Vue({
-  el: "#ep3",
-
-  data: () => ({
-    newName: "",
-    names: ["Ken", "Cassandra", "Steve"],
-    title: "You can only add once",
-    isToggled: false,
-    active: false
-  }),
-
-  methods: {
-    addName() {
-      this.names.push(this.newName);
-      this.newName = "";
-      this.isToggled = true;
-      this.active = true;
-    }
-  }
-});
-
-new Vue({
-  el: "#ep6",
-
-  data: {
-    tasks: [
-      {
-        description: "Assignment 1",
-        isComplete: false
-      },
-      {
-        description: "Assignment 2",
-        isComplete: false
-      },
-      {
-        description: "Report on first day",
-        isComplete: false
-      },
-      {
-        description: "Do House Chores",
-        isComplete: false
-      }
-    ]
-  },
-
-  computed: {
-    incompleteTasks() {
-      return this.tasks.filter(task => !task.isComplete);
-    },
-    completeTasks() {
-      return this.tasks.filter(task => task.isComplete);
-    }
-  }
-});
-
 Vue.component("messages", {
   props: ["title", "body"],
 
@@ -105,9 +43,7 @@ Vue.component("modal", {
 
 new Vue({
   el: "#ep9",
-  data: {
-    showModal: false
-  }
+  data: {}
 });
 
 Vue.component("tabs", {
@@ -167,6 +103,50 @@ Vue.component("tab", {
 });
 
 new Vue({
-  el: "#ep11",
-  data: {}
+  el: "#app",
+  data: () => ({
+    message: "This is Episode 1",
+    newName: "",
+    names: ["Ken", "Cassandra", "Steve"],
+    title: "You can only add once",
+    isToggled: false,
+    active: false,
+    tasks: [
+      {
+        description: "Assignment 1",
+        isComplete: false
+      },
+      {
+        description: "Assignment 2",
+        isComplete: false
+      },
+      {
+        description: "Report on first day",
+        isComplete: false
+      },
+      {
+        description: "Do House Chores",
+        isComplete: false
+      }
+    ],
+    showModal: false
+  }),
+
+  methods: {
+    addName() {
+      this.names.push(this.newName);
+      this.newName = "";
+      this.isToggled = true;
+      this.active = true;
+    }
+  },
+
+  computed: {
+    incompleteTasks() {
+      return this.tasks.filter(task => !task.isComplete);
+    },
+    completeTasks() {
+      return this.tasks.filter(task => task.isComplete);
+    }
+  }
 });
